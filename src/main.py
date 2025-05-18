c04af3c1c0e2808e435314263f8a8e1a1e8f708e
Ihor Dykhta <dikhta.igor@gmail.com>
2025-05-19 01:22:54 +0300

    [feat] fetch quantized terrain
    Signed-off-by: Ihor Dykhta <dikhta.igor@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,3 +1,26 @@
 from fastapi import FastAPI
 from titiler.application.main import app
 
+from rio_tiler.io import Reader
+
+from titiler.core.factory import (
+    TilerFactory
+)
+
+from src.extensions.terrain import terrainExtension
+
+###############################################################################
+# Quantized terrain mesh
+terrain = TilerFactory(
+    reader=Reader,
+    router_prefix="/mesh",
+    extensions=[
+        terrainExtension(max_size=64)
+    ],
+)
+
+app.include_router(
+    terrain.router,
+    prefix="/mesh",
+    tags=["Quantized terrain mesh"],
+)
